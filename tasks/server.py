6e6e71f3c69bcd30d1d83c99ba8b00a2c725966c
Marcus Crane <marcus.crane@xero.com>
2018-05-04 10:20:14 +1200

    Added gunicorn and shortcut

--- tasks/server.py
+++ tasks/server.py
@@ -4,4 +4,4 @@ from invoke import task
 @task
 def start(ctx):
     """ Start the pkgparse server """
-    ctx.run("FLASK_APP=pkgparse/__init__.py flask run --host=0.0.0.0")
+    ctx.run("gunicorn -w 4 -b 0.0.0.0:8000 pkgparse:app")
